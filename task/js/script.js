abd1ece9dbcab8d0d52727e6f0eeff29f621d54e
Jonathan McDaniel <jonmd24@gmail.com>
2014-04-02 16:32:21 -0400

    Styling, Security, Usability, Cleaned Code, Comments,
    Adding styling for initial view and working view,
    Fixed js file to use functions to improve code style,
    Fixed an issue allowing code injection,
    Added user instructions
    Added functionality to js file to improve style and user experience

--- task/js/script.js
+++ task/js/script.js
@@ -1,22 +1,87 @@
+
 $(document).ready(function(){
-	$('#submit').click(function(){
+
+	//Give focus to text field on load
+	$('#taskField').focus();
+
+	//Initially hide the instructions
+	$('#instructions').toggle();
+	
+	//Adds item to task list based on selected priority
+	function addTask(){
 		var task = $('#taskField').val();
+		task = task.replace(/>/g,'&gt;');
+		task = task.replace(/</g,'&lt;');
+		//Ensures that text for task has been entered
 		if (task !== ""){
+			if ($('.item').length === 0){
+				var status = 'top'
+				restyle(status);
+			} 
+			//Checks priority level and sets appropriate style
 			if ($("input[value='low']").is(':checked')){
 				$('#itemList').append("<p class='item' style='background-color: green'>" + task + "</p>")
 			}else if($("input[value='medium']").is(':checked')){
 				$('#itemList').append("<p class='item' style='background-color: orange'>" + task + "</p>")
 			}else{
 				$('#itemList').append("<p class='item' style='background-color: red'>" + task + "</p>")
-			}	
+			}
+			$('#taskField').val('');	
 		}
+	};
+
+	//Adds item to task list for submit button
+	$('#submit').click(function(){
+		addTask();
 	});
+
+	//Removes items from task list
 	$(document).on('click','.item',function(){
-		$(this).animate({
+		$(this).css({
+			'text-decoration': 'line-through'
+		}).animate({
 			opacity: 0
-		}, 250, function(){
+		}, 500, function(){
 			$(this).remove();
+			if ($('.item').length === 0){
+				var status = 'center'
+				restyle(status);
+			} 
 		});
 	});
+
+	//Adds itme to task list for keyboard subbmission
+	$("#taskField").keydown(function(k){
+		if (k.keyCode === 13){
+			stopSubmission = true;
+			addTask();
+		}else{
+			stopSubmission = false;
+		}
+	});
+
+	//Stops page refreshing based on 'enter' being pressed.
+	$("#form1").submit(function(){
+		if (stopSubmission){
+			return false;
+		}
+	});
+
+	//Enables reordering of list items
 	$('#itemList').sortable();
+
+	//Move form div and restyle based on list having items
+	function restyle(status){
+		//Toggles instructions to show appropriate set
+		$('#instructions').toggle();
+		$('#start').toggle();
+		//Changes style based on current status (name of style to change to)
+		if (status === 'top'){
+			$('#formContainer').removeClass('formContainerCenter').addClass('formContainerTop');
+		}else{
+			$('#formContainer').removeClass('formContainerTop').addClass('formContainerCenter');
+		}	
+	}
+
+
 });
